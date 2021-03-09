cb00660e95ac1e7da0dcedd4b7b894273fd9f1e8
smkim104 <soomin30328@gmail.com>
2021-03-09 22:25:47 +0900

    알고리즘 공부

---
+++ PG_멀쩡한사각형.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int gcd(int a, int b) {
+	if (b == 0) {
+		return a;
+	}
+	else {
+		return gcd(b, a % b);
+	}
+}
+long long solution(int w, int h) {
+	long long answer = 1;
+	int temp = gcd(w, h);
+	answer = long(w) * long(h);
+	answer -= long(w) + long(h) - temp;
+
+	return answer;
+}
