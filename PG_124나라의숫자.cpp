b5990e6c99b8bd78bb55ec459efde43a5dc393b8
smkim104 <soomin30328@gmail.com>
2021-03-11 01:19:40 +0900

    프로그래머스 lv2

---
+++ PG_124나라의숫자.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+string answer = "";
+int tmp;
+void find(int a, int tmp) {
+	if (a < 1) {
+		return;
+	}
+	tmp = a % 3;;
+	find((a - 1) / 3, tmp);
+	if (tmp == 1) {
+		answer += "1";
+	}
+	else if (tmp == 2) {
+		answer += "2";
+	}
+	else if (tmp == 0) {
+		answer += "4";
+	}
+
+}
+string solution(int n) {
+	find(n, n % 3);
+	return answer;
+}
